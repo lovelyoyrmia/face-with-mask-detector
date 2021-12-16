3c736d15bd5ef7d96e511e2382c01a477d46b27d
lovelyoyrmia <mokalulovelyo@gmail.com>
2021-12-17 01:33:06 +0700

    added streamlit web app

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from helper_model import prediction_mask
+
+
+def process_image():
+    pass
+
+
+def process_video():
+    pass
+
+# Main App
+
+
+def main():
+    st.title('Face Masked Detection')
+
+
+if __name__ == '__main__':
+    main()
